5b3fa379cb485be1c34e40822818bde61e08ceda
Junjun Zhang <junjun.ca@gmail.com>
2017-06-01 09:41:25 -0400

    added python package to facilitate importing data models into any python code

---
+++ setup.py
@@ -0,0 +1,26 @@
+import os
+from setuptools import setup, find_packages
+
+VERSION_FILE = 'VERSION'
+
+
+def get_version():
+    """Returns the current gdc model version"""
+    current_dir = os.path.dirname(os.path.realpath(__file__))
+    version_path = os.path.join(current_dir, VERSION_FILE)
+
+    with open(version_path, 'r') as f:
+        return f.read().strip()
+
+
+setup(
+    name = 'gdcmodels',
+    version = get_version(),
+    packages = find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+    install_requires = [
+        'PyYAML==3.11'
+    ],
+    package_data = {
+        'gdcmodels': ['es-models/*/*.yaml']
+    }
+)
