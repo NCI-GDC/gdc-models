e6cf9d9b11fded8b1c30db4ba70daca6f3221fad
Greg Singer <singergr@uchicago.edu>
2020-04-22 10:17:27 -0500

    GDCDEVOPS-354 set version with setuptools_scm
    Set the package version using setuptools_scm so we can release new
    versions in the future by just setting a Git tag, without having to
    update setup.py.
    
    Generate dev-requirements.txt using pip-compile so we can pin all the
    specific dependency versions. Add dev-requirements.in to track the
    immediate test dependencies.

--- setup.py
+++ setup.py
@@ -2,15 +2,17 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name='gdcmodels',
-    version='2.1.0',
-    packages=find_packages(exclude=[
-        "scripts", "*-models", "*.tests", "*.tests.*", "tests.*", "tests"]),
+    name="gdcmodels",
+    setup_requires=["setuptools_scm"],
+    use_scm_version={"local_scheme": "dirty-tag"},
+    packages=find_packages(
+        exclude=["scripts", "*-models", "*.tests", "*.tests.*", "tests.*", "tests"]
+    ),
     install_requires=[
-        'PyYAML>=3.11,<6',
-        'elasticsearch>=5.0.0,<6.0.0'
+        "PyYAML>=3.11,<6",
+        "elasticsearch>=5.0.0,<6.0.0",
     ],
     package_data={
-        'gdcmodels': ['es-models/*/*.yaml'],
+        "gdcmodels": ["es-models/*/*.yaml"]
     },
 )
