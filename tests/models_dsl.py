62ba896fb877d988d2342082c43d47f46b6a9287
Rowland Ogwara <rogwara@uchicago.edu>
2022-10-25 11:10:52 -0500

    add tests for exclusions

---
+++ tests/models_dsl.py
@@ -0,0 +1,81 @@
+"""Elasticsearch DSL models that are a subset of the currently supported gdc-models object.
+
+The aim to produce readable to codes that illustrates object nesting within a document.
+"""
+
+from elasticsearch_dsl import Document, Keyword, Nested, Boolean, InnerDoc, MetaField
+
+
+class Observation(InnerDoc):
+    observation_id = Keyword()
+    src_file_id = Keyword()
+    variant_status = Keyword()
+
+
+class CNV(InnerDoc):
+    chromosome = Keyword()
+    cnv_id = Keyword()
+    observation = Nested(Observation)
+
+
+class Civic(InnerDoc):
+    gene_id = Keyword()
+    variant_id = Keyword()
+
+
+class ClinicalAnnotation(InnerDoc):
+    civic = Nested(Civic)
+
+
+class SSM(InnerDoc):
+    chromosome = Keyword()
+    clinical_annotation = Nested(ClinicalAnnotation)
+
+
+class Gene(InnerDoc):
+    biotype = Keyword()
+    gene_id = Keyword()
+    is_cancer_gene_census = Boolean()
+    cnv = Nested(CNV)
+    ssm = Nested(SSM)
+    symbol = Keyword()
+
+
+class CaseCentricWithGeneExcluded(Document):
+    case_id = Keyword()
+    consent_type = Keyword()
+    gene = Nested(Gene)
+
+    class Meta:
+        """Further customizes the index mapping."""
+        dynamic = MetaField("strict")
+        source = MetaField({
+            "excludes": ["gene.*"],
+            "enabled": True
+        })
+        size = MetaField(enabled=True)
+
+    class Index:
+        name = "case_centric__with_gene_excluded"
+        settings = {
+            "index": {
+                "max_result_window": 100000000,
+                "mapping": {
+                    "nested_fields": {"limit": 100},
+                    "nested_objects": {"limit": 100000000},
+                },
+            }
+        }
+
+
+class CaseCentricWithoutDeepNesting(CaseCentricWithGeneExcluded):
+
+    class Meta:
+        dynamic = MetaField("strict")
+        source = MetaField({
+            "excludes": ["gene.*.*"],
+            "enabled": True
+        })
+
+    class Index:
+        name = "case_centric__without_deep_nesting"
---
+++ tests/test_dsls.py
@@ -0,0 +1,89 @@
+"""This module tests the behavior of using excludes in elasticsearch mappings"""
+
+import pytest
+
+from tests import models_dsl as m
+
+
+@pytest.fixture
+def inner_gene_doc(es):
+    cnv_1 = m.CNV(
+        chromosome="chr-1",
+        cnv_id="cnv-1",
+        observation=[
+            m.Observation(
+                observation_id="obs-1", src_file_id="file-id-1", variant_status="ok"
+            )
+        ],
+    )
+    ca_1 = m.ClinicalAnnotation(civic=[m.Civic(gene_id="g1", variant_id="v1")])
+    ssm_1 = m.SSM(chromosome="chr-1", clinical_annotation=[ca_1])
+
+    return m.Gene(
+        biotype="XY", gene_id="gene-id-1", symbol="AX", ssm=[ssm_1], cnv=[cnv_1]
+    )
+
+
+@pytest.fixture
+def case_centric_with_gene_excluded(es, inner_gene_doc):
+    m.CaseCentricWithGeneExcluded.init(using=es)  # create mappings
+
+    case_centric = m.CaseCentricWithGeneExcluded(
+        meta={"id": "case-centric-1"},
+        consent_type="doubt",
+        case_id="d02309ed-045b-435e-a03b-6c7150cff3b8",
+        gene=[inner_gene_doc],
+    )
+
+    case_centric.save(using=es, refresh="wait_for", skip_empty=False)
+
+
+@pytest.fixture
+def case_centric_without_deep_nesting(es, inner_gene_doc):
+    m.CaseCentricWithoutDeepNesting.init(using=es)  # create mappings
+
+    case_centric = m.CaseCentricWithoutDeepNesting(
+        meta={"id": "case-centric-2"},
+        consent_type="allowed",
+        case_id="820a89d0-7480-4dca-a575-6a6637773134",
+        gene=[inner_gene_doc],
+    )
+
+    case_centric.save(using=es, refresh="wait_for", skip_empty=False)
+
+
+@pytest.mark.usefixtures("case_centric_with_gene_excluded")
+def test_index__with_excluded_genes(es):
+    """Test excluding a nested property behaves as expected"""
+
+    response = m.CaseCentricWithGeneExcluded.search(using=es).filter("term", _id="case-centric-1").execute()
+
+    assert len(response.hits) == 1
+    entry = response.hits[0]
+    assert entry.meta.id == "case-centric-1"
+    assert entry.consent_type == "doubt"
+    assert entry.case_id == "d02309ed-045b-435e-a03b-6c7150cff3b8"
+    assert entry.gene == [], "gene should be excluded from result"
+
+
+@pytest.mark.usefixtures("case_centric_without_deep_nesting")
+def test_index__without_deep_nesting(es):
+    """Test excluding a nested property behaves as expected"""
+
+    response = m.CaseCentricWithoutDeepNesting.search(using=es).filter("term", _id="case-centric-2").execute()
+
+    assert len(response.hits) == 1
+    entry = response.hits[0]
+    assert entry.meta.id == "case-centric-2"
+    assert entry.consent_type == "allowed"
+    assert entry.case_id == "820a89d0-7480-4dca-a575-6a6637773134"
+    assert entry.gene != [], "gene should some content"
+
+    assert len(entry.gene) == 1
+    gene_entry = entry.gene[0]
+
+    assert gene_entry.gene_id == "gene-id-1"
+    assert gene_entry.symbol == "AX"
+
+    assert gene_entry.cnv == [], "nested field cnv should be excluded"
+    assert gene_entry.ssm == [], "nested field ssm should be excluded"
