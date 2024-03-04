46c1cbc7917819597e129edcfd348a51179e86df
Micky Byrnes <68259544+BenX00@users.noreply.github.com>
2024-03-04 17:00:19 -0600

    DEV-2420: Move logic in init into sub-modules. (#207)

---
+++ src/gdcmodels/esmodels/__init__.py
@@ -0,0 +1,25 @@
+from gdcmodels.esmodels.models import (
+    Autocomplete,
+    AutocompleteField,
+    DocType,
+    ESMapping,
+    Index,
+    Meta,
+    Models,
+    MutableIndex,
+    Properties,
+    Property,
+)
+
+__all__ = (
+    "Autocomplete",
+    "AutocompleteField",
+    "DocType",
+    "ESMapping",
+    "Index",
+    "Meta",
+    "Models",
+    "MutableIndex",
+    "Properties",
+    "Property",
+)
